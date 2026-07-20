5bb6d13db29af7be81e3826d0f0a4214eec9346f
Pinchuk Dmytro <pinchukdmitro22@gmail.com>
2026-07-20 21:40:40 +0300

    повторное исправление

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -29,7 +29,7 @@ export default function Header() {
           <li className={css.navigationItem}>
             <Link
               className={css.navigationLink}
-              href="/notes/filter/all"
+              href="/notes/filter/All"
             >
               Notes
             </Link>
