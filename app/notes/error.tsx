0f86d5ba6b01fc72f53d372fc8d7f20bf3049757
Pinchuk Dmytro <pinchukdmitro22@gmail.com>
2026-07-20 19:49:35 +0300

    небольшие изменения

--- app/notes/error.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-interface NotesErrorProps {
-  error: Error;
-}
-
-export default function NotesError({ error }: NotesErrorProps) {
-  return <p>Could not fetch the list of notes. {error.message}</p>;
-}
