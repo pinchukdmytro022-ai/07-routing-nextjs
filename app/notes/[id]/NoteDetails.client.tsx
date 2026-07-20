77d9d53407f4c209c5957db1383eb7c1a064d4ec
Pinchuk Dmytro <pinchukdmitro22@gmail.com>
2026-07-20 20:38:38 +0300

    фикс notedetails

--- app/notes/[id]/NoteDetails.client.tsx
+++ app/notes/[id]/NoteDetails.client.tsx
@@ -6,14 +6,16 @@ import { fetchNoteById } from "@/lib/api";
 
 import css from "./NoteDetails.module.css";
 
-
-export default function NoteDetailsClient () {
+export default function NoteDetailsClient() {
   const { id } = useParams<{ id: string }>();
-  const noteId = Number(id);
 
-  const { data: note, isLoading, error } = useQuery({
-    queryKey: ["note", noteId],
-    queryFn: () => fetchNoteById(noteId),
+  const {
+    data: note,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["note", id],
+    queryFn: () => fetchNoteById(id),
     refetchOnMount: false,
   });
 
@@ -27,7 +29,9 @@ export default function NoteDetailsClient () {
           <h2>{note.title}</h2>
           <button className={css.editBtn}>Edit note</button>
         </div>
+
         <p className={css.content}>{note.content}</p>
+
         <p className={css.date}>{note.createdAt}</p>
       </div>
     </div>
